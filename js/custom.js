21fd8e7aa9009a4ec94632da8a95e16ccf9ac128
vashisht03 <kritika.webhopers@gmail.com>
2022-10-11 15:01:37 +0530

    select all

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,13 @@
 $( function() {
   $( "#datepicker" ).datepicker();
 } );
-
+function toggle(source) {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] != source)
+            checkboxes[i].checked = source.checked;
+    }
+}
 // maultioption
   $(document).ready(function() {
 $('.js-example-basic-multiple').select2();
