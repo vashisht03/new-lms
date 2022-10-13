0ddc52e285234bc95f06a4da9390e5f143729b18
vashisht03 <kritika.webhopers@gmail.com>
2022-10-13 13:20:11 +0530

    daterange picker

--- js/custom.js
+++ js/custom.js
@@ -9,9 +9,11 @@ function toggle(source) {
             checkboxes[i].checked = source.checked;
     }
 }
+
 // maultioption
   $(document).ready(function() {
 $('.js-example-basic-multiple').select2();
+
 });
 // var btnToggle = document.querySelector("#header-toggle");
 // var sidebar = document.querySelector(".l-navbar");
@@ -130,3 +132,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
               $(".filter_options").toggle();
             });
           });
+
+          
+          $(function() {
+
+            var start = moment().subtract(29, 'days');
+            var end = moment();
+            
+            function cb(start, end) {
+                $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+            }
+            
+            $('#reportrange').daterangepicker({
+                startDate: start,
+                endDate: end,
+                ranges: {
+                   'Today': [moment(), moment()],
+                   'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                   'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                   'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                   'This Month': [moment().startOf('month'), moment().endOf('month')],
+                   'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                }
+            }, cb);
+            
+            cb(start, end);
+            
+            });
