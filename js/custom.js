df79f85a784e3070baa935465bc21adff7ff68a7
vashisht03 <kritika.webhopers@gmail.com>
2022-10-19 15:39:18 +0530

    team-member html

--- js/custom.js
+++ js/custom.js
@@ -227,3 +227,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
                   return hour.toString()+sp+min.toString();   
                 }
                 
+
+
+                var zoom = 1;
+		
+                $('.zoom').on('click', function(){
+                  zoom += 0.1;
+                  $('.target').css('transform', 'scale(' + zoom + ')');
+                });
+                $('.zoom-init').on('click', function(){
+                  zoom = 1;
+                  $('.target').css('transform', 'scale(' + zoom + ')');
+                });
+                $('.zoom-out').on('click', function(){
+                  zoom -= 0.1;
+                  $('.target').css('transform', 'scale(' + zoom + ')');
+                });
+
+
+
+
+
+                
