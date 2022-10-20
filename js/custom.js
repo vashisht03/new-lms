b0b82484a96f80d2f1a07a783c5d4aaf3a1df5f6
vashisht03 <kritika.webhopers@gmail.com>
2022-10-20 14:30:31 +0530

    leader and memer

--- js/custom.js
+++ js/custom.js
@@ -245,6 +245,57 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 });
 
 
+// drag drop
+const dropzoneSource = document.querySelector(".source");
+const dropzone = document.querySelector(".target");
+const dropzones = [...document.querySelectorAll(".dropzone")];
+const draggables = [...document.querySelectorAll(".draggable")];
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll(".draggable:not(.is-dragging)")
+  ];
+
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+
+      if (offset < 0 && offset > closest.offset) {
+        return {
+          offset,
+          element: child
+        };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
+}
+
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragstart", (e) => {
+    draggable.classList.add("is-dragging");
+  });
+
+  draggable.addEventListener("dragend", (e) => {
+    draggable.classList.remove("is-dragging");
+  });
+});
+
+dropzones.forEach((zone) => {
+  zone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(zone, e.clientY);
+    const draggable = document.querySelector(".is-dragging");
+    if (afterElement === null) {
+      zone.appendChild(draggable);
+    } else {
+      zone.insertBefore(draggable, afterElement);
+    }
+  });
+});
 
 
 
